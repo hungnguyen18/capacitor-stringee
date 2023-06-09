bc354c6984480c4ea2f47f9a1b646d11fbb3753a
Duong-Van-Bao <duongvanbao9109@gmail.com>
2023-06-09 17:14:44 +0700

    feat: install dependency

--- android/src/main/java/vn/wellcare/plugins/capacitor/stringee/Common.java
+++ android/src/main/java/vn/wellcare/plugins/capacitor/stringee/Common.java
@@ -2,7 +2,6 @@ package vn.wellcare.plugins.capacitor.stringee;
 
 import com.stringee.StringeeClient;
 import com.stringee.call.StringeeCall;
-import com.stringee.call.StringeeCall2;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +9,6 @@ import java.util.Map;
 public class Common {
     public static StringeeClient client;
     public static Map<String, StringeeCall> callsMap = new HashMap<>();
-    public static Map<String, StringeeCall2> calls2Map = new HashMap<>();
     public static boolean isInCall = false;
     public static int REQUEST_PERMISSION_CALL = 1;
     public static String TAG = "Stringee";
