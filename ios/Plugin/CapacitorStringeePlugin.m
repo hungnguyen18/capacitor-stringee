e15576442ac61ee389021d7916c5565167136189
dong.nguyen <phong.nguyen@wellcare.vn>
2023-06-11 15:33:26 +0700

    feat: relocate assets

--- ios/Plugin/CapacitorStringeePlugin.m
+++ ios/Plugin/CapacitorStringeePlugin.m
@@ -5,4 +5,7 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorStringeePlugin, "CapacitorStringee",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(StringeeConnect, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(StringeeCall, CAPPluginReturnPromise);
+
 )
