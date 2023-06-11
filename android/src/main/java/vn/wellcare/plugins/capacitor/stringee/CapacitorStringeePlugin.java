b40e4ec27789799e517c5b3c31fdb6f54878b788
phong nguyen <tewnut@gmail.com>
2023-06-11 11:57:22 +0700

    feat: implement stringee connect

--- android/src/main/java/vn/wellcare/plugins/capacitor/stringee/CapacitorStringeePlugin.java
+++ android/src/main/java/vn/wellcare/plugins/capacitor/stringee/CapacitorStringeePlugin.java
@@ -20,88 +20,113 @@ import org.json.JSONObject;
 
 @CapacitorPlugin(name = "CapacitorStringee")
 public class CapacitorStringeePlugin extends Plugin {
+  private Context context;
 
+  public void CapacitorStringee(Context context) {
+    this.context = context;
+  }
 
   @PluginMethod
   public void StringeeConnect(PluginCall call) {
-    String token = call.getString("token");
-    Log.d(Common.TAG, "StringeeConnect : " + call);
-    StringeeConnectionActivity connectionActivity = new StringeeConnectionActivity();
+//    String token = call.getString("token");
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InN0cmluZ2VlLWFwaTt2PTEifQ.eyJqdGkiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCXzE2ODM2ODcwMzA4MDgiLCJpc3MiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCIiwiZXhwIjoxNjgzNzczNDMwODA4LCJ1c2VySWQiOiIzODcyODMiLCJpYXQiOjE2ODM2ODcwMzB9.LHTymQDrGSwTKSAkZfddq-w5S_7Y0M8nAbDjr3jasIo";
+    Log.d(Common.TAG, "StringeeConnect : " + token);
+
+    if (token == null || token.isEmpty()) {
+      call.reject("Token is missing or empty");
+      return;
+    }
+    Context context = getContext();
+    // Create a StringeeConnectionActivity object with the current context
+    StringeeConnectionActivity connectionActivity = new StringeeConnectionActivity(context);
+
+    // Initialize and connect the StringeeClient with the given token
     StringeeClient client = connectionActivity.initAndConnectStringee(token);
-    client.setConnectionListener(new StringeeConnectionListener() {
-      @Override
-      public void onConnectionConnected(StringeeClient stringeeClient, boolean b) {
-        Log.d(Common.TAG, "onConnectionConnected");
-        JSObject ret = new JSObject();
-        ret.put("client", stringeeClient);
-        ret.put("isReconnected", b);
-        notifyListeners("onConnectionConnected", ret);
-      }
 
-      @Override
-      public void onConnectionDisconnected(StringeeClient stringeeClient, boolean isReconnecting) {
-        Log.d(Common.TAG, "onConnectionDisconnected");
-        JSObject ret = new JSObject();
-        ret.put("client", stringeeClient);
-        ret.put("isReconnecting", isReconnecting);
-        notifyListeners("onConnectionDisconnected", ret);
-      }
+    if (client != null) {
+      // Connection succeeded, return success result
+      JSObject result = new JSObject();
+      result.put("message", "Connected to Stringee server");
+      call.resolve(result);
+      client.setConnectionListener(new StringeeConnectionListener() {
+        @Override
+        public void onConnectionConnected(StringeeClient stringeeClient, boolean b) {
+          Log.d(Common.TAG, "onConnectionConnected");
+          JSObject ret = new JSObject();
+          ret.put("client", stringeeClient);
+          ret.put("isReconnected", b);
+          notifyListeners("onConnectionConnected", ret);
+        }
 
-      @Override
-      public void onIncomingCall(StringeeCall stringeeCall) {
-        Log.d(Common.TAG, "onIncomingCall: callId - " + stringeeCall.getCallId());
-        JSObject ret = new JSObject();
-        ret.put("callId", stringeeCall.getCallId());
-        notifyListeners("onIncomingCall", ret);
+        @Override
+        public void onConnectionDisconnected(StringeeClient stringeeClient, boolean isReconnecting) {
+          Log.d(Common.TAG, "onConnectionDisconnected");
+          JSObject ret = new JSObject();
+          ret.put("client", stringeeClient);
+          ret.put("isReconnecting", isReconnecting);
+          notifyListeners("onConnectionDisconnected", ret);
+        }
 
-      }
+        @Override
+        public void onIncomingCall(StringeeCall stringeeCall) {
+          Log.d(Common.TAG, "onIncomingCall: callId - " + stringeeCall.getCallId());
+          JSObject ret = new JSObject();
+          ret.put("callId", stringeeCall.getCallId());
+          notifyListeners("onIncomingCall", ret);
 
-      @Override
-      public void onIncomingCall2(StringeeCall2 stringeeCall2) {
-        Log.d(Common.TAG, "onIncomingCall2: callId - " + stringeeCall2.getCallId());
-        JSObject ret = new JSObject();
-        ret.put("callId", stringeeCall2.getCallId());
-        notifyListeners("onIncomingCall", ret);
-      }
+        }
 
-      @Override
-      public void onConnectionError(StringeeClient stringeeClient, StringeeError stringeeError) {
-        Log.d(Common.TAG, "onConnectionError: " + stringeeError.getMessage());
-        JSObject ret = new JSObject();
-        ret.put("client", stringeeClient);
-        ret.put("message", stringeeError.getMessage());
-        ret.put("error", stringeeError);
-        notifyListeners("onConnectionError", ret);
-      }
+        @Override
+        public void onIncomingCall2(StringeeCall2 stringeeCall2) {
+          Log.d(Common.TAG, "onIncomingCall2: callId - " + stringeeCall2.getCallId());
+          JSObject ret = new JSObject();
+          ret.put("callId", stringeeCall2.getCallId());
+          notifyListeners("onIncomingCall", ret);
+        }
 
-      @Override
-      public void onRequestNewToken(StringeeClient stringeeClient) {
-        Log.d(Common.TAG, "onRequestNewToken");
-        JSObject ret = new JSObject();
-        ret.put("client", stringeeClient);
-        notifyListeners("onRequestNewToken", ret);
-        // Get new token here and connect to Stringe server
-      }
+        @Override
+        public void onConnectionError(StringeeClient stringeeClient, StringeeError stringeeError) {
+          Log.d(Common.TAG, "onConnectionError: " + stringeeError.getMessage());
+          JSObject ret = new JSObject();
+          ret.put("client", stringeeClient);
+          ret.put("message", stringeeError.getMessage());
+          ret.put("error", stringeeError);
+          notifyListeners("onConnectionError", ret);
+        }
 
-      @Override
-      public void onCustomMessage(String from, JSONObject msg) {
-        Log.d(Common.TAG, "onCustomMessage: from - " + from + " - msg - " + msg);
-        JSObject ret = new JSObject();
-        ret.put("from", from);
-        ret.put("msg", msg);
-        notifyListeners("onCustomMessage", ret);
-      }
+        @Override
+        public void onRequestNewToken(StringeeClient stringeeClient) {
+          Log.d(Common.TAG, "onRequestNewToken");
+          JSObject ret = new JSObject();
+          ret.put("client", stringeeClient);
+          notifyListeners("onRequestNewToken", ret);
+          // Get new token here and connect to Stringe server
+        }
+
+        @Override
+        public void onCustomMessage(String from, JSONObject msg) {
+          Log.d(Common.TAG, "onCustomMessage: from - " + from + " - msg - " + msg);
+          JSObject ret = new JSObject();
+          ret.put("from", from);
+          ret.put("msg", msg);
+          notifyListeners("onCustomMessage", ret);
+        }
 
 
-      @Override
-      public void onTopicMessage(String from, JSONObject msg) {
-        Log.d(Common.TAG, "onTopicMessage: from - " + from + " - msg - " + msg);
-        JSObject ret = new JSObject();
-        ret.put("from", from);
-        ret.put("msg", msg);
-        notifyListeners("onTopicMessage", ret);
-      }
-    });
+        @Override
+        public void onTopicMessage(String from, JSONObject msg) {
+          Log.d(Common.TAG, "onTopicMessage: from - " + from + " - msg - " + msg);
+          JSObject ret = new JSObject();
+          ret.put("from", from);
+          ret.put("msg", msg);
+          notifyListeners("onTopicMessage", ret);
+        }
+      });
+    } else {
+      // Connection failed, return error result
+      call.reject("Unable to connect to Stringee server");
+    }
+
   }
 
   // @PluginMethod
