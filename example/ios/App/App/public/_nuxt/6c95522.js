1823c8c6ca011cc3536d8032e143647fb0e6d003
dong.nguyen <phong.nguyen@wellcare.vn>
2023-06-11 14:51:40 +0700

    feat: copy sample code and initizte ios

--- example/ios/App/App/public/_nuxt/6c95522.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[64],{781:function(n,e,o){"use strict";o.r(e),o.d(e,"CapacitorStringeeWeb",(function(){return c}));var t=o(658);class c extends t.a{async echo(n){return await console.log("ECHO",n),n}}}}]);
-//# sourceMappingURL=6c95522.js.map
--- example/ios/App/App/public/_nuxt/b497205.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[51],{778:function(n){n.exports=JSON.parse('{"name":"Capacitor Plugin mẫu"}')}}]);
-//# sourceMappingURL=b497205.js.map
