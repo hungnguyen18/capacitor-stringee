829818bb3f6a8ab43f86e8497499d6c190d45e35
Duong-Van-Bao <duongvanbao9109@gmail.com>
2023-06-12 14:27:36 +0700

    update stringee android

--- example/src/pages/index.vue
+++ example/src/pages/index.vue
@@ -5,8 +5,8 @@
       <v-btn @click="connect">Connect</v-btn>
       <v-text-field v-model="callFrom" label="call from"></v-text-field>
       <v-text-field v-model="callTo" label="call to"></v-text-field>
-      <v-btn :disabled="isAuth" color="primary" @click="call">Call</v-btn>
-      <v-btn :disabled="isAuth" color="error" @click="reject">Reject</v-btn>
+      <v-btn :disabled="!isAuth" color="primary" @click="call">Call</v-btn>
+      <v-btn :disabled="!isAuth" color="error" @click="reject">Reject</v-btn>
       <p>Status: {{ status }}</p>
       <w-debug-log />
     </v-container>
@@ -21,20 +21,31 @@ export default defineComponent({
   setup() {
     const { log } = useLog()
     const token = ref(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InN0cmluZ2VlLWFwaTt2PTEifQ.eyJqdGkiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCXzE2ODM2ODcwMzA4MDgiLCJpc3MiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCIiwiZXhwIjoxNjgzNzczNDMwODA4LCJ1c2VySWQiOiIzODcyODMiLCJpYXQiOjE2ODM2ODcwMzB9.LHTymQDrGSwTKSAkZfddq-w5S_7Y0M8nAbDjr3jasIo'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InN0cmluZ2VlLWFwaTt2PTEifQ.eyJqdGkiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCXzE2ODY1NTM2NjMyOTkiLCJpc3MiOiJTS3NXTXlPZjhqUm51c3dPS1pEOXI5alZtVHE2bXB2MzhCIiwiZXhwIjoxNjg2NjQwMDYzMjk5LCJ1c2VySWQiOiIzODcyODMiLCJpYXQiOjE2ODY1NTM2NjN9.-Dgu4PEwnb581Hgmh3Y-FImUwoYVuvGPdxBNHI_sDvU'
     )
     const callFrom = ref('387283')
-    const callTo = ref('84348375392')
+    const callTo = ref('84329221173')
     const status = ref('')
-    const isAuth = ref(false)
+    const isAuth = ref(true)
     const StringeePlugin = CapacitorStringee
     const connect = () => {
       if (process.client)
-        StringeePlugin.StringeeConnect(token.value, onClientEvent)
+        StringeePlugin.StringeeConnect(
+          {
+            token: token.value
+          },
+          onClientEvent
+        )
     }
     const call = () => {
       if (process.client)
-        StringeePlugin.StringeeCall(callFrom.value, callTo.value, onCallEvent)
+        StringeePlugin.StringeeCall(
+          {
+            callFrom: callFrom.value,
+            callTo: callTo.value
+          },
+          onCallEvent
+        )
     }
     const reject = () => {
       if (process.client)
